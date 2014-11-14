d8597ba6a0561b72b8393a6f7cfa78bfe0cce9df
Vikas Kedia <vikas.kedia@gmail.com>
2014-11-14 08:00:00 +0530

    a

--- src/com/claims/db/DbConnector.java
+++ src/com/claims/db/DbConnector.java
@@ -10,12 +10,30 @@ import javax.sql.DataSource;
 
 public class DbConnector {
 	
-	public static Connection getConnection() throws SQLException, ClassNotFoundException, NamingException {
+	public static Connection getConnection() {
+		return getConnectionFromDriver();
+	}
+	
+	public static Connection getConnectionFromDs() throws SQLException, ClassNotFoundException, NamingException {
 		Connection con = null;
 		InitialContext context = new InitialContext();
 		DataSource datasource = (DataSource) context.lookup("java:jboss/datasources/ClaimData");
 		con = datasource.getConnection();
 		System.out.println("Connection is " + con);
+		return con;
+	}
+	
+	public static Connection getConnectionFromDriver() {
+		String url = "jdbc:mysql://natasha230.cdmticztqqlw.us-east-1.rds.amazonaws.com:3306/";
+		String dbName = "claims";
+		String userName = "natasha";
+		String password = "rkt220685";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url+dbName,userName,password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return con;
 	}
 	
